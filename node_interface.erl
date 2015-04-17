1725f5f6610124a51d45a57f4de5bc9b1557f498
Kjetil Kjeka <kjetilkjeka@gmail.com>
2015-04-17 17:01:34 +0200

    more ad-hoc stuff

--- node_interface.erl
+++ node_interface.erl
@@ -1,18 +1,18 @@
--module(node_interface.erl)
--compile(export_all)
+-module(node_interface).
+-compile(export_all).
 
 -define(SEND_PORT, 5667).
 -define(RECV_PORT, 5677).
 -define(COOKIE, "erlang"). 
--define(SEEK_FREQUENCY, 500).
+-define(SEEK_PERIOD, 2000).
 
 start_listening_for_connections() ->
-    {ok, Socket} = gen_udp:open(?RECV_PORT, [binary, {active,true}]),
-    listen_for_connections().
+    {ok, Socket} = gen_udp:open(?RECV_PORT, [binary, {active,false}]),
+    listen_for_connections(Socket).
 
 listen_for_connections(RecvSocket) ->
-    {ok, {Adress, ?SEND_PORT, ?COOKIE}} = gen_udp:recv(RecvSocket, {255.255.255.255}, ?RECV_PORT),
-    case Adress of
+    {ok, {Adress, ?SEND_PORT, ?COOKIE}} = gen_udp:recv(RecvSocket, 0),
+    io:format("Adress is found -ish~n", []).
 	
 
 
@@ -20,10 +20,9 @@ start_seeking_listeners() ->
     {ok, Socket} = gen_udp:open(?SEND_PORT, [binary, {active,true}]),
     seek_listeners(Socket).
 seek_listeners(SendSocket) ->
-    gen_udp:send(SendSocket, {255.255.255.255}, ?RECV_PORT, ?COOKIE),
-    timer:wait(?SEEK_FREQUENCY),
+    ok = gen_udp:send(SendSocket, {255,255,255,255}, ?RECV_PORT, ?COOKIE),
+    io:format("UDP shizzle sent ~n", []),
+    timer:sleep(?SEEK_PERIOD),
     seek_listeners(SendSocket).
 
-
-ping_cluster() ->
     
